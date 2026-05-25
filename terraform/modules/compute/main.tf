3f379e16db96a926440c6de9790593d8ceb937be
flytothemoon <rei.saga@outlook.com>
2026-05-25 21:49:50 +0800

    Update health_grace_period

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -131,7 +131,7 @@ resource "aws_autoscaling_group" "web" {
   vpc_zone_identifier       = [var.public_subnet_id]
   target_group_arns         = [var.target_group_arn]
   health_check_type         = "ELB"
-  health_check_grace_period = 300
+  health_check_grace_period = 600
 
   min_size         = var.asg_min_size
   max_size         = var.asg_max_size
